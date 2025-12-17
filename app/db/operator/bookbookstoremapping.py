7a0c274e93c0779f60b6ab8a5c76796a07282a1c
Jackson <jackson97042@gmail.com>
2025-12-17 15:57:20 +0800

    feat: add update books post router

--- app/db/operator/bookbookstoremapping.py
+++ app/db/operator/bookbookstoremapping.py
@@ -5,6 +5,16 @@ from sqlalchemy import select, and_, update, insert
 from app.db.models.book_bookstore_mapping import BookBookstoreMapping
 
 
+async def get_book_mapping_by_mapping_id(
+    db: AsyncSession, book_bookstore_mapping_id: UUID
+) -> Optional[BookBookstoreMapping]:
+    stmt = select(BookBookstoreMapping).where(
+        BookBookstoreMapping.book_bookstore_mapping_id == book_bookstore_mapping_id
+    )
+    result = await db.execute(stmt)
+    return result.scalars().one_or_none()
+
+
 async def get_book_mapping(
     db: AsyncSession, book_id: UUID, bookstore_id: UUID
 ) -> Optional[BookBookstoreMapping]:
@@ -37,6 +47,32 @@ async def create_book_bookstore_mapping(
         .values(
             book_id=book_id, bookstore_id=bookstore_id, price=price, store_quantity=store_quantity
         )
+        .returning(BookBookstoreMapping.book_bookstore_mapping_id)
+    )
+
+    result = await db.execute(query)
+
+    return result.scalar_one()
+
+
+async def update_book_bookstore_mapping(
+    db: AsyncSession,
+    book_bookstore_mapping_id: UUID,
+    price: Optional[int] = None,
+    store_quantity: Optional[int] = None,
+):
+    values = {}
+
+    if price is not None:
+        values["price"] = price
+
+    if store_quantity is not None:
+        values["store_quantity"] = store_quantity
+
+    query = (
+        update(BookBookstoreMapping)
+        .values(values)
+        .where(BookBookstoreMapping.book_bookstore_mapping_id == book_bookstore_mapping_id)
         .returning(BookBookstoreMapping.book_bookstore_mapping_id)
     )
 
