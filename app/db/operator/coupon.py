666463b5a4e0062b6a07443e1472db061cab0836
Jackson <jackson97042@gmail.com>
2025-12-18 00:28:40 +0800

    fix: lazy loading not allowed in async session without extra settings

--- app/db/operator/coupon.py
+++ app/db/operator/coupon.py
@@ -1,10 +1,11 @@
 from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 from app.db.models.coupon import Coupon
 
 
 async def get_coupon_by_id(db: AsyncSession, coupon_id: UUID):
-    query = select(Coupon).where(Coupon.coupon_id == coupon_id)
+    query = select(Coupon).where(Coupon.coupon_id == coupon_id).options(joinedload(Coupon.staff))
     result = await db.execute(query)
     return result.scalars().one_or_none()
